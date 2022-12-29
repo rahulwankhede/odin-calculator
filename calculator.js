73f57631ec54dc4b2b0f5fbc6bb058d7ef800ecc
Rahul Wankhede <rahulwankhede@github.com>
2022-12-29 16:27:37 +0530

    add listeners to numbers and operators

--- calculator.js
+++ calculator.js
@@ -21,3 +21,30 @@ function operate(operator, a, b){
 	else if (operator === '/') return divide(a, b);
 	else return 'Undefined operator';
 }
+
+const input = document.querySelector('.input');
+const output = document.querySelector('.output');
+
+function appendNumberToInput(c){
+	input.textContent += c;
+}
+
+function appendOperatorToInput(c){
+	input.textContent = ['+', '-', '*', '/'].includes(input.textContent.slice(-1)) ? input.textContent.slice(0, -1) + c : input.textContent + c;
+}
+
+const numbers = document.querySelectorAll('div.numbers button');
+
+for(let button of numbers){
+	button.addEventListener('click', function(e){
+		appendNumberToInput(button.id);
+	});
+}
+
+const operators = document.querySelectorAll('div.operators button');
+
+for(let operator of operators){
+	operator.addEventListener('click', function(e){
+		appendOperatorToInput(operator.id);
+	});
+}
